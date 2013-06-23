82786578e1bc9d6dccdbb82ad90f726bc8cfaa94
Brickever <maybeoneday@iwillgivemy.email>
2013-06-23 11:16:45 -0400

    Commented code, modified a few variable names

--- LUCA.py
+++ LUCA.py
@@ -33,7 +33,7 @@ minver = ""
 os.system("title {0} v{1}".format(app, majver))
 localUserName = input("\nEnter your Creation Lab Username: ")
 
-
+# Search the localUserName on the Creation Lab
 url = "http://universe.lego.com/en-us/community/creationlab/displaycreationlist.aspx?SearchText={0}&order=oldest&show=12".format(localUserName)
 r = requests.get(url).content
 soup = BeautifulSoup(r)
@@ -42,13 +42,15 @@ for link in soup.find_all('a'):
     if link.get('href')[0:49] == "/en-us/Community/CreationLab/DisplayCreation.aspx": 
         creations.append('http://universe.lego.com' + link.get('href'))
 
-#Check if links were found/added for the entered username 
+# Check if links were found/added for the entered username
+# If not, close LUCA 
 if not creations:
     print('The username "{0}" does not result in any search entry on the Creation Lab.'.format(localUserName))
     input("Press Enter to close LUCA.")
     raise SystemExit(0) 
         
-
+# Check if one link contains the localUserName
+# If not, close LUCA
 r = requests.get(creations[0]).content
 soup = BeautifulSoup(r)
 onlineUserName = soup.find(id="ctl00_ContentPlaceHolderUniverse_HyperLinkUsername")
@@ -56,10 +58,7 @@ if localUserName.lower() == onlineUserName.string.lower():
     memberid = onlineUserName.get('href')[63:99]
     print("\nYour Creations are now downloading, {0}.\n".format(localUserName))
 else:
-    # The username entered could not be found online
-    # TODO: update message to include onlineUserName or memberid,
-    # or does this mean the username does not exist?
-    print('The username "{0}" does not match with the one online.'.format(localUserName))
+    print('The username "{0}" does not appear to match with any usernames online.'.format(localUserName))
     input("Press Enter to close LUCA.")
     raise SystemExit(0) 
 
@@ -85,14 +84,13 @@ for link in soup.find_all('a'):
 
 for creation in creations:
 
-    r = requests.get(creation)
-    status = r.status_code
-    creationPage = r.content
-    soup = BeautifulSoup(creationPage)
+# ------- INFORMATION GHATERING ------- #
+    r = requests.get(creation).content
+    soup = BeautifulSoup(r)
 
     title = soup.find_all('h1')[2]   #add .string to get only the text
-    titleT = soup.find_all('h1')[2].string
-    titleT = titleT.replace('/','')
+    titleString = title.string
+    titleString = titleString.replace('/','')
     description = soup.find(id="creationInfoText")
     tags = soup.find_all(class_='column-round-body')[3].contents[9]
     challenge = soup.find(id="CreationChallenge").contents[1].contents[1]
@@ -115,53 +113,45 @@ for creation in creations:
 {5}
 </body>
 </html>
-    '''.format(titleT, title, description, tags, challenge, date)
+    '''.format(titleString, title, description, tags, challenge, date)
 
-    # Old code
-    #imgM = soup.find("a", id="ctl00_ContentPlaceHolderUniverse_HyperLinkMainImage").get('href')
-    #imgM = "http://universe.lego.com/en-us/community/creationlab/" + imgM
-
-    imgT = []
+    imgLinkList = []
     i = 1
+    
+    for imgLink in soup.find_all('a'):
+        if imgLink.get('href')[0:13] == "GetMedia.aspx":
+            imgLinkList.append('http://universe.lego.com/en-us/community/creationlab/'+ imgLink.get('href'))
 
-    for img in soup.find_all('a'):
-        if img.get('href')[0:13] == "GetMedia.aspx":
-            imgT.append('http://universe.lego.com/en-us/community/creationlab/'+ img.get('href'))
 
-    # --- BEGIN NEW CODE --- #
+# ------- INFORMATION WRITING ------- #
     # List of illegal characters for filenames
     blacklist = ["\\", "/", ":", "*", "?", '"', "<", ">", "|"]
     # Makes it easier to understand what is going on.
     filepath = os.path.join(os.getcwd(), localUserName)
-    # --- END NEW CODE --- #
 
-    for imgLink in imgT:
+
+    for imgLink in imgLinkList:
         r = requests.get(imgLink)
         img = r.content            
-
-        # --- BEGIN NEW CODE --- #
+       
         # Original filename  
-        filename = "{0}{1}.jpg".format(titleT, i)
+        filename = "{0}{1}.jpg".format(titleString, i)
         for char in blacklist:  
             if char in filename:
                 # If an illegal character is found, replace it with a dash
                 filename = filename.replace(char, "-")
 
-
         # Write all non HTML files.  
         with open(os.path.join(filepath, filename), 'wb') as newImg:
             newImg.write(img)
-       # --- END NEW CODE --- #
-
+      
         # Display filename after it was installed, 
         # part of LUCA's non-GUI progress bar.
         print(os.path.basename(filename), end="\n")
         i = i + 1
 
-    # --- BEGIN NEW CODE --- #
-    # TODO: Is supposed to do the same thing as non-HTML stuff,
-    # but it fails for some reason.
-    HTMLfilename = "{0}.html".format(titleT)
+
+    HTMLfilename = "{0}.html".format(titleString)
     for char in blacklist:  
         if char in HTMLfilename:
             HTMLfilename = HTMLfilename.replace(char, "-")
@@ -169,7 +159,6 @@ for creation in creations:
     # Write HTML documents.        
     with open(os.path.join(filepath, HTMLfilename), "wt") as newHTML:
         newHTML.write(page)
-    # --- END NEW CODE --- #
 
     # Display filename after it was installed, 
     # part of LUCA's non-GUI progress bar.
@@ -184,7 +173,7 @@ if "Thumbs.db" in num_of_files:
 
 # Display success message containing number
 # of files downloaded and where they were saved.
-print('\n{0} files successfully downloaded and saved to \n"{1}"'.format(len(num_of_files), os.path.join(os.getcwd(), localUserName)))
+print('\n{0} files successfully downloaded and saved to \n"{1}"'.format(len(num_of_files), filepath))
 input("\nPress Enter to close LUCA.\n")
 raise SystemExit(0)
     
